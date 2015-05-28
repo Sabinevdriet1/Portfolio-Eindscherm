b099762868fb2bccd954302ebb1e6f6f6ff53296
Sabine <sabinevdriet@gmail.com>
2015-05-28 16:12:55 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var ageSelect = document.getElementById('age');
+var sendButton = document.getElementById('send');
+
+sendButton.onclick = function(){
+var websiteUrl = ageSelect.value;
+document.location.href = websiteUrl;
+}
