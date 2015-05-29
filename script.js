0a9eba034bda533abc7b05f800ad3a81df802908
Sabine <sabinevdriet@gmail.com>
2015-05-29 19:33:24 +0200

    Added document load

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
-var ageSelect = document.getElementById('age');
-var sendButton = document.getElementById('send');
+window.onload = function(e){ 
+  var ageSelect = document.getElementById('age');
+  var sendButton = document.getElementById('send');
 
-sendButton.onclick = function(){
-var websiteUrl = ageSelect.value;
-document.location.href = websiteUrl;
+  sendButton.onclick = function(){
+    var websiteUrl = ageSelect.value;
+    document.location.href = websiteUrl;
+  }
 }
+
