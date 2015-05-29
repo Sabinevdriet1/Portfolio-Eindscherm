9d7608484f70b580a47d51bdb44a324bdc4ff23e
Sabine <sabinevdriet@gmail.com>
2015-05-29 19:40:30 +0200

    Removed onload

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
+var ageSelect = document.getElementById('age');
+var sendButton = document.getElementById('send');
 
-document.body.onload = function(e){ 
-  var ageSelect = document.getElementById('age');
-  var sendButton = document.getElementById('send');
-
-  sendButton.onclick = function(){
-    var websiteUrl = ageSelect.value;
-    document.location.href = websiteUrl;
-  }
+sendButton.onclick = function(){
+  var websiteUrl = ageSelect.value;
+  document.location.href = websiteUrl;
 }
 
